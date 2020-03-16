75cdeb5c56190e2b162f876d779d7930ce0b8d04
Ahmad Sabry <ahmadsabry330@gmail.com>
2020-03-16 22:54:47 +0200

    Tweeks

--- handlers/index.js
+++ handlers/index.js
@@ -1,20 +1,20 @@
 const callSendAPI = require("../SendApi")
 const text = require("../templates/text")
 const buttonList = require("../templates/buttonList")
-// const genericList = require("../templates/genericList")
+const genericList = require("../templates/genericList")
 
 // // Triggered when user sends a message
-// const handleMessage = (sender_psid, received_message) => {
-//     let response = {
-//         "messaging_type": "RESPONSE",
-//         "recipient": {
-//             "id": sender_psid
-//         },
-//         "message": genericList(received_message.text)
-//     }
+const handleMessage = (sender_psid, received_message) => {
+    let response = {
+        "messaging_type": "RESPONSE",
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": genericList(received_message.text)
+    }
 
-//     callSendAPI(sender_psid, response)
-// }
+    callSendAPI(sender_psid, response)
+}
 
 // Triggered when user presses a button
 const handlePostback = (sender_psid, received_message) => {
@@ -30,6 +30,6 @@ const handlePostback = (sender_psid, received_message) => {
 }
 
 module.exports = {
-    // handleMessage,
+    handleMessage,
     handlePostback
 }
