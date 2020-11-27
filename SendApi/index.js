6c3d4b9d2f312791c1c4e6bbb6852f4e9974905f
Ahmad Sabry <ahmadsabry330@gmail.com>
2020-11-27 14:10:06 +0200

    Tweeks

--- SendApi/index.js
+++ SendApi/index.js
@@ -2,6 +2,7 @@ const fetch = require("node-fetch")
 
 //  Send the responce to messenger
 module.exports = async response => {
+    console.log("responseresponse", response)
     const url = `https://graph.facebook.com/v5.0/me/messages?access_token=${process.env.ACCESS_TOKEN}`
     try {
         await fetch(url, {
--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/webhook", (req, res) => {
             // mark message as seen
             await callSendAPI(markAsRead(sender_psid))
 
-            console.log(webhook_event.message)
+            console.log("webhook_event.postback", webhook_event.postback)
 
             if (webhook_event.message) {
                 handle.handleMessage(sender_psid, webhook_event.message.text)
